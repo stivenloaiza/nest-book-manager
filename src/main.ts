384dd1f8949f36c08ce09e2bc00642fb782632f9
Milton Loaiza <loaizadeveloper@gmail.com>
2024-04-25 14:52:54 -0500

    Configs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+  .setTitle('Book Manager')
+  .setDescription('API to books management')
+  .setVersion('1.0')
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-doc', app, document);
+
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
